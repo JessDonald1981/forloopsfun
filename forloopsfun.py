e6cdfcd307a46b1896f765eee5231c2d0c245c78
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-27 21:32:28 -0700

    loops file

---
+++ forloopsfun.py
@@ -0,0 +1,43 @@
+# for num in range(1,21):
+#     print num
+
+# # range(1,21):
+# #     print range 
+
+# for num in range (1,21):
+#     print num 
+#     print "hello" 
+#     if (13)
+#     
+
+###################################
+
+# While Loop Exercises
+# 1
+# num = 1 
+# while (num < 21):
+#     print num 
+#     num = num+1
+
+# 2
+# num = 1 
+# while (num < 21):
+#     print num
+#     num = num + 1
+
+#     if num == 13:
+#         print "hello"
+
+# 3
+# num = 0 
+# while (num < 101):
+#     print num
+#     num = num+10
+
+# 4 
+# fruits=["apples", "bananas", "oranges"]
+# index = 0
+# while (index < len(fruits)):
+#     print fruits[index]
+#     index = index + 1
+
